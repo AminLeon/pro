bc20d5da5f0f5e2c257cb55ded308a987fc9c346
ivan9989-pkj <santanaquintanai@gmail.com>
2023-02-10 19:45:35 +0000

    first commit

---
+++ src/Cadenas.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+public class Cadenas {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+    //Ejercicio 1.Escribir por pantalla cada carácter de una cadena introducida por teclado.
+
+
+        /*System.out.println("Introduzca una palabra: ");
+        String texto = sc.nextLine();
+        for(int i =0; i<texto.length(); i++){
+            String subtext=texto.substring(i,i+1);
+            System.out.println("Su cadena esta compuesta por : "   + subtext);
+        }*/
+
+    //Ejercicio 2 Realizar un programa que comprueba si una cadena leída por teclado comienza por una subcadena introducida por teclado.
+
+        /*System.out.println("Introduce una cadena: ");
+        String cadena = sc.nextLine();
+        System.out.println("Introduce la sub cadena: ");
+        String subcadena = sc.nextLine();
+
+        if (cadena.startsWith(subcadena)){
+            System.out.println("La cadena empieza por la subcadena indicada");
+        }else{
+            System.out.println("La cadena no empieza por la subcadena indicada");
+        }*/
+
+    //Ejercicio 3 .Pide una cadena y un carácter por teclado (valida que sea un carácter) y muestra cuantas veces aparece el carácter en la cadena.
+
+       /* System.out.println("Introduzca una cadena de caracteres: ");
+        String a = sc.nextLine();
+        int a2 = a.length();
+        System.out.println("Introduzca el caracter que quiera comprobar: ");
+        char b = sc.next().charAt(0);
+        int contador = 0;
+        for(int i =0; i<a2; i++){
+            char d = a.charAt(i);
+            if (d==b){
+                contador++;
+            }
+        }
+        System.out.println("El caracter" + b + " se repite " + contador + " veces ");*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Ejercicio 4 .Suponiendo que hemos introducido una cadena por teclado que representa una frase (palabras separadas por espacios), realiza un programa que cuente cuantas palabras tiene.
+    //Ejercicio 5 Si tenemos una cadena con un nombre y apellidos, realizar un programa que muestre las iniciales en mayúsculas.
+    //Ejercicio 6 Realizar un programa que dada una cadena de caracteres por caracteres, genere otra cadena resultado de invertir la primera.
+    //Ejercicio 7 Pide una cadena y dos caracteres por teclado (valida que sea un carácter), sustituye la aparición del primer carácter en la cadena por el segundo carácter
+    //Ejercicio 8 Realizar un programa que lea una cadena por teclado y convierta las mayúsculas a minúsculas y viceversa.
+    //Ejercicio 9 Realizar un programa que compruebe si una cadena contiene una subcadena. Las dos cadenas se introducen por teclado.
+    //Ejercicio 10 Introducir una cadena de caracteres e indicar si es un palíndromo. Una palabra palíndroma es aquella que se lee igual adelante que atrás. Ej: Roma, amor
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
